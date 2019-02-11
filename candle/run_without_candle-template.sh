492cb404cf25a9b3e495b0385eb9ec90c34723b3
Andrew Weisman <andrew.weisman@nih.gov>
2019-02-11 01:59:41 -0500

    Renamed the run_without_candle.sh script to run_without_candle-template.sh

--- candle/run_without_candle-template.sh
+++ candle/run_without_candle-template.sh
@@ -2,16 +2,15 @@
 
 #SBATCH --partition=gpu
 #SBATCH --mem=20G
-#SBATCH --gres=gpu:v100:1
-#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:k80:1
+#SBATCH --time=12:00:00
 #SBATCH --job-name=hpset_23
 
 # Set up environment
 module load python/3.6
-CANDLE=/data/BIDS-HPC/public/candle
 
 # Set the file that the Python script below will read in order to determine the model parameters
 export DEFAULT_PARAMS_FILE=/home/weismanal/notebook/2019-01-28/jobs/not_candle/single_param_set.txt
 
 # Run the model
-python $CANDLE/Supervisor/workflows/examples/unet.py
+python /home/weismanal/checkouts/fnlcr-bids-hpc/models/unet.py
