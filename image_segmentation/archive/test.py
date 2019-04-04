6d9badc78500d314eb0fce90f48d8bdddfa09e70
Andrew Weisman <andrew.weisman@nih.gov>
2019-04-04 01:28:47 -0400

    Reorganized image_segmentation package

---
+++ image_segmentation/archive/test.py
@@ -0,0 +1,28 @@
+# In Bash: module load python/3.6
+import sys
+sys.path.append('/home/weismanal/checkouts/fnlcr-bids-hpc/image_segmentation/packages')
+from image_segmentation import testing
+#testing.load_data()
+#testing.calculate_metrics()
+#testing.output_metrics()
+#testing.make_plots()
+
+
+
+# # Constants
+# plotting_transposes = ((0,1,2),(0,2,1),(0,2,1))
+# dirs = ['x','y','z']
+
+# # Variable
+# plotting_transpose = plotting_transposes[dirs.index(view)]
+
+# # Do some transposing just to make the plotting a reasonable orientation so we don't have to turn our heads 90 degrees when we look at the movies
+# known_masks = np.transpose(known_masks,plotting_transpose)
+# unpadded_shape = (unpadded_shape[plotting_transpose[0]],unpadded_shape[plotting_transpose[1]],unpadded_shape[plotting_transpose[2]])
+# images = np.transpose(images,plotting_transpose)
+# inferred_masks = np.transpose(inferred_masks,plotting_transpose)
+
+# # Make the data RGBA
+# images_rgba = imseg_utils.gray2rgba(images)
+# known_masks_rgba = imseg_utils.gray2rgba(known_masks,A=0.2*255,shade_color=[0,0,1],makeBGTransp=True)
+# inferred_masks_rgba = imseg_utils.gray2rgba(inferred_masks,A=0.2*255,shade_color=[1,0,0],makeBGTransp=True)
--- image_segmentation/test.py
+++ image_segmentation/test.py
@@ -1,28 +1,24 @@
-# In Bash: module load python/3.6
+# In Bash (only on Biowulf): module load python/3.6
 import sys
-sys.path.append('/home/weismanal/checkouts/fnlcr-bids-hpc/image_segmentation/packages')
+sys.path.append('/home/weismanal/checkouts/fnlcr-bids-hpc/image_segmentation/packages') # only on Biowulf
+#sys.path.append('/Users/weismanal/checkouts/fnlcr-bids-hpc/image_segmentation/packages') # only on laptop
 from image_segmentation import testing
-#testing.load_data()
-#testing.calculate_metrics()
-#testing.output_metrics()
-#testing.make_plots()
 
+# Test the helper functions in the testing module
+#print(testing.define_data())
+#print(testing.load_data())
 
+# Test the image_augmentation module
+#testing.testing__image_augmentation__augment_images()
 
-# # Constants
-# plotting_transposes = ((0,1,2),(0,2,1),(0,2,1))
-# dirs = ['x','y','z']
+# Test the post_inference module
+#testing.testing__post_inference__load_inferred_masks()
+#testing.testing__post_inference__calculate_metrics()
+#testing.testing__post_inference__output_metrics()
+#testing.testing__post_inference__make_movies_individual_masks()
 
-# # Variable
-# plotting_transpose = plotting_transposes[dirs.index(view)]
-
-# # Do some transposing just to make the plotting a reasonable orientation so we don't have to turn our heads 90 degrees when we look at the movies
-# known_masks = np.transpose(known_masks,plotting_transpose)
-# unpadded_shape = (unpadded_shape[plotting_transpose[0]],unpadded_shape[plotting_transpose[1]],unpadded_shape[plotting_transpose[2]])
-# images = np.transpose(images,plotting_transpose)
-# inferred_masks = np.transpose(inferred_masks,plotting_transpose)
-
-# # Make the data RGBA
-# images_rgba = imseg_utils.gray2rgba(images)
-# known_masks_rgba = imseg_utils.gray2rgba(known_masks,A=0.2*255,shade_color=[0,0,1],makeBGTransp=True)
-# inferred_masks_rgba = imseg_utils.gray2rgba(inferred_masks,A=0.2*255,shade_color=[1,0,0],makeBGTransp=True)
+# Test the aggregate_masks module
+testing.testing__aggregate_masks__copy_planes()
+#testing.testing__aggregate_masks__get_plane_copies()
+#testing.testing__aggregate_masks__generate_aggregate_masks()
+#testing.testing__aggregate_masks__make_movies_aggregate_masks()
