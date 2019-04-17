9ec07d7033a466b1016a6fa25759137dca697c46
Andrew Weisman <andrew.weisman@nih.gov>
2019-04-17 05:20:24 -0400

    Modified relative paths since I moved the testing scripts to their own subdirectory

--- image_segmentation/test/testing.py
+++ image_segmentation/test/testing.py
@@ -17,9 +17,9 @@ def load_data():
     # Loads data specifically for testing
 
     # Import relevant modules
-    from .post_inference import load_inferred_masks
+    from ..post_inference import load_inferred_masks
     import numpy as np
-    from . import utils
+    from .. import utils
 
     # Get data definitions
     roi_list, data_dir, models, inference_directions = define_data()
@@ -42,10 +42,10 @@ def testing__image_augmentation__augment_images():
     # Instructions on installing and running the module are located at https://cbiit.github.io/fnlcr-bids-hpc/image_segmentation/packages/image_segmentation/
 
     # Import relevant modules
-    from .image_augmentation import augment_images
+    from ..image_augmentation import augment_images
     from skimage import io
     import numpy as np
-    from . import utils
+    from .. import utils
     import os
 
     # Constant
@@ -103,14 +103,14 @@ def testing__post_inference__load_inferred_masks():
     roi_list, data_dir, models, inference_directions = define_data()
 
     # Test function of interest
-    from .post_inference import load_inferred_masks
+    from ..post_inference import load_inferred_masks
     import numpy as np
     inferred_masks_list = []
     for roi in roi_list:
         inferred_masks_list.append(load_inferred_masks(roi, np.load(data_dir+'/'+roi+'_input_img.npy').shape, models, inference_directions, data_dir, do_output=False)) # these are ultimately uint8
 
     # Check output by eye
-    from . import utils
+    from .. import utils
     utils.arr_info(inferred_masks_list[0])
 
 def testing__post_inference__calculate_metrics():
@@ -120,7 +120,7 @@ def testing__post_inference__calculate_metrics():
     __, known_masks_list, inferred_masks_list, __, __ = load_data()
 
     # Test the function of interest
-    from .post_inference import calculate_metrics
+    from ..post_inference import calculate_metrics
     metrics_2d_list, __ = calculate_metrics(known_masks_list, inferred_masks_list)
 
     # Just make sure this output looks reasonable; if so, the rest is probably good too
@@ -132,11 +132,11 @@ def testing__post_inference__output_metrics():
 
     # Load necessary data
     __, known_masks_list, inferred_masks_list, roi_list, models = load_data()
-    from .post_inference import calculate_metrics
+    from ..post_inference import calculate_metrics
     __, metrics_3d_list = calculate_metrics(known_masks_list, inferred_masks_list)
 
     # Test function of interest; it will output metrics_3d.html and metrics_3d.txt
-    from .post_inference import output_metrics
+    from ..post_inference import output_metrics
     output_metrics(metrics_3d_list, roi_list, models)
 
 def testing__post_inference__make_movies_individual_masks():
@@ -144,11 +144,11 @@ def testing__post_inference__make_movies_individual_masks():
 
     # Load necessary data
     images_list, known_masks_list, inferred_masks_list, roi_list, models = load_data()
-    from .post_inference import calculate_metrics
+    from ..post_inference import calculate_metrics
     metrics_2d_list, metrics_3d_list = calculate_metrics(known_masks_list, inferred_masks_list)
     
     # Test function of interest; it will output multiple .mp4 movies
-    from .post_inference import make_movies_individual_masks
+    from ..post_inference import make_movies_individual_masks
     make_movies_individual_masks(roi_list, images_list, inferred_masks_list, models, nframes=40, known_masks_list=known_masks_list, metrics_2d_list=metrics_2d_list, metrics_3d_list=metrics_3d_list)
 
 def testing__aggregate_masks__copy_planes():
@@ -158,11 +158,11 @@ def testing__aggregate_masks__copy_planes():
     __, __, inferred_masks_list, __, __ = load_data()
 
     # Test function of interest
-    from .aggregate_masks import copy_planes
+    from ..aggregate_masks import copy_planes
     plane_copy = copy_planes(inferred_masks_list[0][0,0,:,:,:], inferred_masks_list[0][0,0,:,:,:] & inferred_masks_list[0][0,1,:,:,:] & inferred_masks_list[0][0,2,:,:,:]) # copy_planes(masks, seed)
 
     # Check by eye
-    from . import utils
+    from .. import utils
     utils.arr_info(plane_copy)
 
 def testing__aggregate_masks__get_plane_copies():
@@ -173,12 +173,12 @@ def testing__aggregate_masks__get_plane_copies():
 
     # Test function of interest
     masks = inferred_masks_list[0][0,:,:,:,:].astype('bool')
-    from .aggregate_masks import get_plane_copies
+    from ..aggregate_masks import get_plane_copies
     import numpy as np
     plane_copies = get_plane_copies(masks, np.where(np.sum(masks, axis=0).astype('uint8')>=2)) # [3,3,Z,X,Y] (bool); get_plane_copies(masks, seed_tuple)
 
     # Check by eye
-    from . import utils
+    from .. import utils
     utils.arr_info(plane_copies)
 
 def testing__aggregate_masks__generate_aggregate_masks():
@@ -188,11 +188,11 @@ def testing__aggregate_masks__generate_aggregate_masks():
     __, __, inferred_masks_list, __, __ = load_data()
 
     # Test function of interest
-    from .aggregate_masks import generate_aggregate_masks
+    from ..aggregate_masks import generate_aggregate_masks
     aggregate_masks_list, new_names = generate_aggregate_masks(inferred_masks_list)
 
     # Check by eye
-    from . import utils
+    from .. import utils
     utils.arr_info(aggregate_masks_list[0])
     print(new_names)
 
@@ -201,17 +201,17 @@ def testing__aggregate_masks__make_movies_aggregate_masks():
 
     # Load necessary data
     images_list, known_masks_list, inferred_masks_list, roi_list, models = load_data()
-    from .aggregate_masks import generate_aggregate_masks
+    from ..aggregate_masks import generate_aggregate_masks
     aggregate_masks_list, new_names = generate_aggregate_masks(inferred_masks_list)
-    from .post_inference import calculate_metrics
+    from ..post_inference import calculate_metrics
     metrics_2d_list, metrics_3d_list = calculate_metrics(known_masks_list, aggregate_masks_list, nviews=3)
     
     # Test the function of interest; this should generate multiple .mp4 movies in a new movies directory
-    from .aggregate_masks import make_movies_aggregate_masks
+    from ..aggregate_masks import make_movies_aggregate_masks
     make_movies_aggregate_masks(roi_list, images_list, aggregate_masks_list, models, nframes=40, known_masks_list=known_masks_list, metrics_2d_list=metrics_2d_list, metrics_3d_list=metrics_3d_list, new_names=new_names)
 
     # Output the 3D metrics .html and .txt files to check
-    from .post_inference import output_metrics
+    from ..post_inference import output_metrics
     output_metrics(metrics_3d_list, roi_list, models, new_names=new_names)
 
     # Also output some metrics to check by eye
