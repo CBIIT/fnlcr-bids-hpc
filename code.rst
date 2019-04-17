726500581f3ea6a474f5404219c79d7ef0172711
Andrew Weisman <andrew.weisman@nih.gov>
2019-04-17 05:02:48 -0400

    Made a bunch of mostly documentation changes for George

---
+++ code.rst
@@ -0,0 +1,7 @@
+Documentation for the the fnlcr-bids-hpc GitHub repository
+**********************************************************
+
+.. automodule:: image_segmentation
+
+.. automodule:: image_segmentation.image_augmentation
+   :members:
--- conf.py
+++ conf.py
@@ -10,16 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'FNLCR-BIDS-HPC'
-copyright = '2019, Andrew Weisman and George Zaki'
-author = 'Andrew Weisman and George Zaki'
+copyright = '2019, BIDS-HPC'
+author = 'BIDS-HPC'
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,7 +27,9 @@ author = 'Andrew Weisman and George Zaki'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+#extensions = [
+#]
+extensions = ['sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
