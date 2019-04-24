a7e40ed958524fed17403cc8410f83c5493ec3dd
Andrew Weisman <andrew.weisman@nih.gov>
2019-04-24 02:16:03 -0400

    Deleted old make_history_obj.py script

--- scripts/make_history_obj.py
+++
@@ -1,6 +0,0 @@
-class HistoryDummy:
-    def __init__(self, mynum):
-        self.history = {'val_loss': mynum-1, 'val_corr': mynum+1, 'val_dice_coef': mynum+2}
-
-history = HistoryDummy(15)
-print(history.history['val_loss'])
