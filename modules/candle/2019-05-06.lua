f1400ef7089c4fbb1f974b683781d3e0ca56ec54
Andrew Weisman <andrew.weisman@nih.gov>
2019-05-08 01:28:42 -0400

    Updated CANDLE modulefiles

--- modules/candle/2019-05-06.lua
+++ modules/candle/2019-05-06.lua
@@ -1,13 +1,12 @@
- -- This version uses OpenMPI v3.1.2 as a Biowulf module
- -- Note that the versions of the checkouts in this version are from 2019-05-06
+ -- This version uses MPICH v3.3, built by Andrew
 
-whatis("Version: 2019-04-04")
+whatis("Version: 2019-05-06")
 whatis("URL: https://cbiit.github.io/fnlcr-bids-hpc/documentation/candle")
 whatis("Description: Open source software for scalable hyperparameter optimization")
 
 local app         = "candle"
-local version     = "2019-04-04"
-local base = "/data/BIDS-HPC/public/software/distributions/candle/2019-04-04"
+local version     = "2019-05-06"
+local base = "/data/BIDS-HPC/public/software/distributions/candle/2019-05-06"
 
 setenv("CANDLE", base) -- used by submit_candle_job.sh, run_without_candle.sh, and copy_candle_template.sh
 append_path("PATH", pathJoin(base,"scripts")) -- used only in order to find the copy_candle_template script
