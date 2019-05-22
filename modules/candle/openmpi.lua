bebc49f731e06806f85f71d2e00173dea1af7d02
Andrew Weisman <andrew.weisman@nih.gov>
2019-05-22 03:38:30 -0400

    Updated modules, renaming them mostly and updating a couple paths, added a default version in .version

--- modules/candle/openmpi.lua
+++ modules/candle/openmpi.lua
@@ -1,15 +1,17 @@
- -- This is the development distribution, which can be understood by observing the symbolic links in /data/BIDS-HPC/public/software/distributions/candle/dev
+ -- This version uses OpenMPI v3.1.2 as a Biowulf module
+ -- This used to be called 2019-04-04
+ -- Note that the versions of the checkouts in this version are from 2019-05-06 and for Supervisor from 2019-05-22
 
-whatis("Version: dev")
+whatis("Version: openmpi")
 whatis("URL: https://cbiit.github.io/fnlcr-bids-hpc/documentation/candle")
 whatis("Description: Open source software for scalable hyperparameter optimization")
 
 local app         = "candle"
-local version     = "dev"
-local base = "/data/BIDS-HPC/public/software/distributions/candle/dev"
+local version     = "openmpi"
+local base = "/data/BIDS-HPC/public/software/distributions/candle/openmpi"
 
 setenv("CANDLE", base) -- used by submit_candle_job.sh, run_without_candle.sh, and copy_candle_template.sh
--- append_path("PATH", pathJoin(base,"scripts")) -- used only in order to find the copy_candle_template script
+ -- append_path("PATH", pathJoin(base,"scripts")) -- used only in order to find the copy_candle_template script
 append_path("PATH", pathJoin(base,"Supervisor/templates/scripts")) -- used only in order to find the copy_candle_template script
 setenv("SITE", "biowulf") -- used by submit_candle_job.sh
 
