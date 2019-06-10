5e5a56512098fa5f509d8715bf850c51a158e8f9
George Zaki <george.zaki@nih.gov>
2019-06-10 10:57:23 -0400

    Updated functionality to check individual augmentations only

--- packages/image_segmentation/test/test-hitif/test-well07/test-fov.py
+++ packages/image_segmentation/test/test-hitif/test-well07/test-fov.py
@@ -4,13 +4,10 @@ from image_augmentation import augment_images
 import utils
 from skimage import io
 import os
+import augment_hitif
 from augment_hitif import hitif_aug
 
 augmenter = hitif_aug("configuration-well07.cfg")
-
-#file_name="AssayPlate_PerkinElmer_CellCarrier-384 Ultra_B07_T0001F002L01A01Z01C01.tif"
-#fov_dir="/data/HiTIF/data/dl_segmentation_input/HiTIF_colorectal/input/exp2"
-#gt_dir="/data/HiTIF/data/dl_segmentation_input/HiTIF_colorectal/ground_truth/exp2"
 
 file_name="Row0-NNI-1by1.tif"
 fov_dir="."
@@ -23,21 +20,23 @@ gt_file=os.path.join(gt_dir, file_name)
 images = io.imread(image_file)[:255, :255]
 masks = io.imread(gt_file)[:255, :255]
 
-#print("Images")
-#utils.arr_info(images)
+print("Images")
+utils.arr_info(images)
 
 
-#print("MASKS")
-#utils.arr_info(masks)
+print("MASKS")
+utils.arr_info(masks)
 
-#print(images.shape)
-#print(masks.shape)
+print(images.shape)
+print(masks.shape)
 
 augment_images(images, masks, 1, do_composite=False, AugSettings= augmenter, output_dir="augmented-samples-well07")
-image_aug, mask_aug = augment_images(images, masks, 200, do_composite=True, AugSettings= augmenter, output_dir="augmented-samples-well07")
+augment_images(images, masks, 1, do_composite=False, AugSettings= augmenter, output_dir="augmented-samples-well07")
+#image_aug, mask_aug = augment_images(images, masks, 200, do_composite=True, AugSettings= augmenter, output_dir="augmented-samples-well07")
 
-io.imsave('augmented.tif',image_aug)
-io.imsave('masks.tif',mask_aug)
-rgba1 = utils.arr2rgba(image_aug,A=255,shade_color=[1,1,1],makeBGTransp=False)
-rgba2 = utils.arr2rgba(mask_aug,A=round(0.25*255),shade_color=[1,0,0],makeBGTransp=True)
-io.imsave('output-overlay.tif',utils.overlay_images(rgba1, rgba2))
+#
+#io.imsave('augmented.tif',image_aug)
+#io.imsave('masks.tif',mask_aug)
+#rgba1 = utils.arr2rgba(image_aug,A=255,shade_color=[1,1,1],makeBGTransp=False)
+#rgba2 = utils.arr2rgba(mask_aug,A=round(0.25*255),shade_color=[1,0,0],makeBGTransp=True)
+#io.imsave('output-overlay.tif',utils.overlay_images(rgba1, rgba2))
